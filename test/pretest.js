5793d30eee995b219ee72bf7dbc229ad19d1d1f4
Gabriel Giacomini <gabrielgiacomini@gmail.com>
2025-07-19 15:04:08 -0300

    Enhance MongoDB connection handling across multiple files by adding useNewUrlParser, useUnifiedTopology, and useCreateIndex options. Update pretest.js, server.js, and getMongooseConnection.js to ensure compatibility with the latest MongoDB driver features, improving connection stability and reducing deprecation warnings.

--- test/pretest.js
+++ test/pretest.js
@@ -1,18 +1,25 @@
-var mongoose = require('mongoose');
-var mongoUri = 'mongodb://localhost/test';
+var mongoose = require("mongoose");
+var mongoUri = "mongodb://localhost/test";
 
 function dropTestDatabase(done) {
-	mongoose.connect(mongoUri, { useMongoClient: true }, function (err) {
-		if (!err) {
-			mongoose.connection.db.dropDatabase(function (err) {
-				mongoose.connection.close(function (err) {
-					done(err);
+	mongoose.connect(
+		mongoUri,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		},
+		function (err) {
+			if (!err) {
+				mongoose.connection.db.dropDatabase(function (err) {
+					mongoose.connection.close(function (err) {
+						done(err);
+					});
 				});
-			});
-		} else {
-			done(err);
+			} else {
+				done(err);
+			}
 		}
-	});
+	);
 }
 
 function pretestTasks() {
